ed1331fa87c628929cbbca779c106e2b35d3c7fb
johnnyg-git <55810567+johnnyg-git@users.noreply.github.com>
2025-04-12 17:00:01 +0100

    Change MelonInfo name to ExampleAxe

--- Examples/ExampleAxe/AssemblyInfo.cs
+++ Examples/ExampleAxe/AssemblyInfo.cs
@@ -2,8 +2,7 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using MelonLoader;
-using JS1Framework;
 
 using ExampleAxe;
 
-[assembly: MelonInfo(typeof(EntryPoint), "JS1Framework", "1.0.0", "Johnny Johnny")]
+[assembly: MelonInfo(typeof(EntryPoint), "ExampleAxe", "1.0.0", "Johnny Johnny")]
