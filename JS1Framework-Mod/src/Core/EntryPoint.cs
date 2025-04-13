0a45ee268fbceecbfe0c5cd5cc677b797091d393
johnnyg-git <55810567+johnnyg-git@users.noreply.github.com>
2025-04-13 20:26:43 +0100

    Stop patching Harmony because MelonLoader does this on its own

--- JS1Framework-Mod/src/Core/EntryPoint.cs
+++ JS1Framework-Mod/src/Core/EntryPoint.cs
@@ -10,38 +10,11 @@ namespace JS1Framework.Core
 {
     internal class EntryPoint : MelonMod
     {
-        private const string HarmonyId = "com.johnnyjohnny.js1framework";
-        private static HarmonyLib.Harmony harmony;
-
-        static void HarmonyPatch()
-        {
-            MelonLogger.Msg("Starting Harmony patching...");
-
-            harmony = new HarmonyLib.Harmony(HarmonyId);
-            harmony.PatchAll();
-
-            MelonLogger.Msg("Harmony patching complete.");
-        }
-
-        static void HarmonyUnpatch()
-        {
-            MelonLogger.Msg("Starting Harmony unpatching...");
-
-            // Only unpatch if harmony has actually been initialized
-            if (harmony != null)
-            {
-                harmony.UnpatchSelf();
-                harmony = null;
-            }
-
-            MelonLogger.Msg("Harmony unpatching complete.");
-        }
-
         public override void OnInitializeMelon()
         {
             MelonLogger.Msg("Loading JS1Framework...");
 
-            HarmonyPatch();
+            // Do stuff in the future
 
             MelonLogger.Msg("JS1Framework loaded successfully.");
         }
@@ -50,7 +23,7 @@ namespace JS1Framework.Core
         {
             MelonLogger.Msg("Unloading JS1Framework...");
 
-            HarmonyUnpatch();
+            // Do stuff in the future
 
             MelonLogger.Msg("JS1Framework unloaded successfully.");
         }
